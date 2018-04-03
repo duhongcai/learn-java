d160a879be041513924c2c0ba2bcd16b11864060
阿杜 <duhongcaisb@163.com>
2018-04-03 11:26:31 +0800

    init project

---
+++ source/main/java/cn/qtec/learn/Demo.java
@@ -0,0 +1,10 @@
+package cn.qtec.learn;
+
+/**
+ * Created by duhc on 2018/4/3.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
